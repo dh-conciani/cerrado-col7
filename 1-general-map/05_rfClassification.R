6a950862f9957fafd946a0322bde60f4e1e2e5ee
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-04-29 09:00:29 -0300

    Update 05_rfClassification.R

--- 1-general-map/05_rfClassification.R
+++ 1-general-map/05_rfClassification.R
@@ -12,6 +12,9 @@ output_version <-  '1'   # output classification version
 ## define hyperparameters for then rf classifier
 n_tree <- 250
 
+## define output asset
+output_asset <- 'projects/mapbiomas-workspace/COLECAO6/classificacao-test/'
+
 ## read landsat mosaic 
 mosaic <- ee$ImageCollection('projects/nexgenmap/MapBiomas2/LANDSAT/BRAZIL/mosaics-2')$
   filterMetadata('biome', 'equals', 'CERRADO')
@@ -24,3 +27,21 @@ regions_vec <- ee$FeatureCollection('users/dh-conciani/collection7/classificatio
 
 ## define regions to be processed 
 regions_list <- sort(unique(regions_vec$aggregate_array('mapb')$getInfo()))
+
+## get predictor names to be used in the classification
+bands <- mosaic$first()$bandNames()$getInfo()
+
+## remove bands with 'cloud' or 'shade' into their names
+bands <- bands[- which(sapply(strsplit(bands, split='_', fixed=TRUE), function(x) (x[1])) == 'cloud' |
+                         sapply(strsplit(bands, split='_', fixed=TRUE), function(x) (x[1])) == 'shade') ]
+
+## add auxiliary bandnames
+bands <- c(bands, 'latitude', 'longitude_sin', 'longitude_cos', 'hand', 'amp_ndvi_3yr')
+
+## define assets
+### training samples (prefix string)
+training_samples <- 'users/dh-conciani/collection7/training/'
+
+### classification regions (imageCollection, one region per image)
+regions_ic <- 'users/dh-conciani/collection7/classification_regions/eachRegion'
+
