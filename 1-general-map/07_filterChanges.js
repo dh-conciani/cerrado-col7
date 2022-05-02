10a5ae6d82ad42ba374a1496f7a6c29b42033b18
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-05-02 15:50:05 -0300

    Create 07_filterChanges.js

---
+++ 1-general-map/07_filterChanges.js
@@ -0,0 +1,25 @@
+// filter spurious transitions by using number of changes and number of classes 
+// for clarification write to dhemerson.costa@ipam.org.br
+
+// set root imageCollection
+var root = 'users/dh-conciani/collection7/c7-general-post/';
+
+// set version
+var input_version = '1';
+var output_version = '1';
+
+// define input file 
+var file_in = 'CERRADO_col7_gapfill_v' + input_version;
+
+// import mapbiomas color ramp
+var vis = {
+    'min': 0,
+    'max': 49,
+    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification6')
+};
+
+// load input
+var classification = ee.Image(root + file_in);
+Map.addLayer(classification.select(['classification_2021']), vis, 'classification');
+
+// compute number of changes; and number of classes
