9415d6162fc030f2eef56051dcc78f14e2886a5b
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-05-04 16:25:46 -0300

    Update 07_Incidence.js

--- 1-general-map/07_Incidence.js
+++ 1-general-map/07_Incidence.js
@@ -23,14 +23,9 @@ var classification = ee.Image(root + file_in);
 
 // compute number of classes and changes 
 var nChanges = classification.reduce(ee.Reducer.countRuns()).subtract(1).rename('number_of_changes');
-var nClasses = classification.reduce(ee.Reducer.countDistinctNonNull()).rename('number_of_classes');
 
 // get the count of connections
 var connected_nChanges = nChanges.connectedPixelCount({
-      'maxSize': 100, 
-      'eightConnected': false});
-
-var connected_nClasses = nClasses.connectedPixelCount({
       'maxSize': 100, 
       'eightConnected': false});
 
@@ -44,13 +39,46 @@ Map.addLayer(nChanges, {palette: ["#C8C8C8", "#FED266", "#FBA713", "#cb701b", "#
 
 Map.addLayer(connected_nChanges, {palette: ['green', 'yellow', 'orange', 'red'], min:0, max:10}, 'con. nChanges', false);
 
-// number of classes
-Map.addLayer(nClasses, {palette: [ "#ffffff", "#C8C8C8", "#AE78B2", "#772D8F", "#4C226A", "#22053A"],
-                                  min: 0, max: 5}, 'number of classes', false);
-                                  
-Map.addLayer(connected_nClasses, {palette: ['green', 'yellow', 'orange', 'red'], min:0, max:3}, 'con. nClasses', false);
-
 // classification
 Map.addLayer(mode, vis, 'mode', false);
 Map.addLayer(classification.select(['classification_2021']), vis, 'classification');
-Map.addLayer(classification, {}, 'all', false);
+
+// get border pixels (high geolocation RMSE) to be masked by the mode
+var border_mask = connected_nChanges.lte(6).and(nChanges.gt(12));
+border_mask = border_mask.updateMask(border_mask.eq(1));    
+
+// get borders to rectfy
+var rect_border = mode.updateMask(border_mask);
+//Map.addLayer(rect_border, vis, 'border to rect');
+
+// get forests to rectfy
+var forest = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(3));
+forest = ee.Image(3).updateMask(forest);
+//Map.addLayer(forest, vis, 'forest');
+
+// get savanna to rectfy 
+var savanna = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(4));
+savanna = ee.Image(4).updateMask(savanna);
+
+// get wetland to rectfy 
+var wetland = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(11));
+wetland = ee.Image(11).updateMask(wetland);
+
+// get grassland to rectfy 
+var grassland = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(12));
+grassland = ee.Image(12).updateMask(grassland);
+
+// get pasture to rectfy 
+var pasture = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(15));
+pasture = ee.Image(15).updateMask(pasture);
+
+// get agriculture to rectfy 
+var agriculture = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(19));
+agriculture = ee.Image(19).updateMask(agriculture);
+
+// get mosaic to rectfy 
+var mosaic = connected_nChanges.gt(6).and(nChanges.gt(12)).and(mode.eq(21));
+mosaic = ee.Image(21).updateMask(mosaic);
+
+// build rect mask
+
