2cd33223686055140efb26ac5a94318548b0e8ba
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-05-16 09:56:32 -0300

    Update 03_getAccuracy.R

--- 1-general-map/utils/analyses/03_getAccuracy.R
+++ 1-general-map/utils/analyses/03_getAccuracy.R
@@ -23,9 +23,10 @@ file_name <- c(#'CERRADO_col7_gapfill_v1',
   #'CERRADO_col7_gapfill_incidence_v1',
   #'CERRADO_col7_gapfill_incidence_temporal_v1',
   #'CERRADO_col7_gapfill_incidence_temporal_v2',
-  'CERRADO_col7_gapfill_incidence_temporal_v3'
+  #'CERRADO_col7_gapfill_incidence_temporal_v3'
   #'CERRADO_col7_gapfill_incidence_temporal_spatial_v2',
   #'CERRADO_col7_gapfill_incidence_temporal_spatial_v3'
+  'CERRADO_col7_gapfill_incidence_temporal_frequency_v3'
   )
 
 ## set output path (local)
