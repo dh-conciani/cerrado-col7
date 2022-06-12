a4a3ac9e19aa30f6341090c7a89a7368eb7923a7
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-06-12 10:51:45 -0300

    Update 01_getArea.js

--- 1-general-map/utils/analyses/01_getArea.js
+++ 1-general-map/utils/analyses/01_getArea.js
@@ -27,7 +27,8 @@ var asset = [
   //root + 'CERRADO_col7_gapfill_incidence_temporal_frequency_v6'
   //root + 'CERRADO_col7_gapfill_incidence_temporal_v7'
   //root + 'CERRADO_col7_gapfill_incidence_v8'
-  root + 'CERRADO_col7_gapfill_incidence_temporal_v8'
+  //root + 'CERRADO_col7_gapfill_incidence_temporal_v8'
+  root + 'CERRADO_col7_gapfill_incidence_temporal_frequency_v8'
   ];
 
 // define classification regions 
